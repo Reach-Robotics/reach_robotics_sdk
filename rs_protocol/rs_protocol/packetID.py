323c692add15ff48e62f40389db4a8b48c89856d
Kyle McLean <k.mclean@blueprintlab.com>
2025-04-09 16:53:38 +1000

    refactor packetID.py

--- rs_protocol/rs_protocol/packetID.py
+++ rs_protocol/rs_protocol/packetID.py
@@ -4,111 +4,137 @@ class PacketID():
 
     # Request
     REQUEST = 0x60
-    HEARTBEAT_PACKETS = 0x91
-    HEARTBEAT_FREQUENCY_HZ = 0x92
-
     PacketType[REQUEST] = int
-    PacketType[HEARTBEAT_PACKETS] = int
-    PacketType[HEARTBEAT_FREQUENCY_HZ] = int
-    
-    # Control 
-    MODE = 0x01
-    RESET = 0xFD
 
-    VELOCITY = 0x02 
+    HEARTBEAT_PACKETS = 0x91
+    PacketType[HEARTBEAT_PACKETS] = int
+
+    HEARTBEAT_FREQUENCY_HZ = 0x92
+    PacketType[HEARTBEAT_FREQUENCY_HZ] = int
+
+    # Control
+    MODE = 0x01
+    PacketType[MODE] = int
+
+    RESET = 0xFD
+    PacketType[RESET] = int
+
+    VELOCITY = 0x02
+    PacketType[VELOCITY] = float
+
     POSITION = 0x03
-    CURRENT = 0x05 
+    PacketType[POSITION] = float
+
+    CURRENT = 0x05
+    PacketType[CURRENT] = float
 
     INDEXED_RELATIVE_POSITION = 0x0D
+    PacketType[INDEXED_RELATIVE_POSITION] = float
 
     INVERSE_KINEMATICS_GLOBAL_POSITION = 0xA1
-    INVERSE_KINEMATICS_GLOBAL_VELOCITY = 0xA2 
-    INVERSE_KINEMATICS_LOCAL_VELOCITY = 0xCB
-
-    PacketType[MODE] = int
-    PacketType[RESET] = int
-    PacketType[VELOCITY] = float
-    PacketType[POSITION] = float
-    PacketType[CURRENT] = float
-    PacketType[INDEXED_RELATIVE_POSITION] = float
     PacketType[INVERSE_KINEMATICS_GLOBAL_POSITION] = float
+
+    INVERSE_KINEMATICS_GLOBAL_VELOCITY = 0xA2
     PacketType[INVERSE_KINEMATICS_GLOBAL_VELOCITY] = float
+
+    INVERSE_KINEMATICS_LOCAL_VELOCITY = 0xCB
     PacketType[INVERSE_KINEMATICS_LOCAL_VELOCITY] = float
-    
+
     # Preset Positions
     POSITION_PRESET_GO = 0x55
+    PacketType[POSITION_PRESET_GO] = int
+
     POSITION_PRESET_CAPTURE = 0x56
+    PacketType[POSITION_PRESET_CAPTURE] = int
 
     POSITION_PRESET_SET_0 = 0x57
+    PacketType[POSITION_PRESET_SET_0] = float
+
     POSITION_PRESET_SET_1 = 0x58
+    PacketType[POSITION_PRESET_SET_1] = float
+
     POSITION_PRESET_SET_2 = 0x59
+    PacketType[POSITION_PRESET_SET_2] = float
+
     POSITION_PRESET_SET_3 = 0x5A
+    PacketType[POSITION_PRESET_SET_3] = float
 
     POSITION_PRESET_NAME_0 = 0x5B
-    POSITION_PRESET_NAME_1 = 0x5C
-    POSITION_PRESET_NAME_2 = 0x5D
-    POSITION_PRESET_NAME_3 = 0x5E
-
-    PacketType[POSITION_PRESET_GO] = int
-    PacketType[POSITION_PRESET_CAPTURE] = int
-    PacketType[POSITION_PRESET_SET_0] = float
-    PacketType[POSITION_PRESET_SET_1] = float
-    PacketType[POSITION_PRESET_SET_2] = float
-    PacketType[POSITION_PRESET_SET_3] = float
     PacketType[POSITION_PRESET_NAME_0] = float
+
+    POSITION_PRESET_NAME_1 = 0x5C
     PacketType[POSITION_PRESET_NAME_1] = float
+
+    POSITION_PRESET_NAME_2 = 0x5D
     PacketType[POSITION_PRESET_NAME_2] = float
+
+    POSITION_PRESET_NAME_3 = 0x5E
     PacketType[POSITION_PRESET_NAME_3] = float
 
     # Configure
-    SAVE = 0x50 
-    POSITION_LIMITS = 0x10 
-    VELOCITY_LIMITS = 0x11
-    CURRENT_LIMITS = 0x12
-
+    SAVE = 0x50
     PacketType[SAVE] = int
+
+    POSITION_LIMITS = 0x10
     PacketType[POSITION_LIMITS] = float
+
+    VELOCITY_LIMITS = 0x11
     PacketType[VELOCITY_LIMITS] = float
+
+    CURRENT_LIMITS = 0x12
     PacketType[CURRENT_LIMITS] = float
 
+    ETH_PARAMETER = 0x46
+    PacketType[ETH_PARAMETER] = int
+
     # Workspace Restrictions
-    BOX_OBSTACLE_02 = 0xA5 
+    BOX_OBSTACLE_02 = 0xA5
+    PacketType[BOX_OBSTACLE_02] = float
+
     BOX_OBSTACLE_03 = 0xA6
+    PacketType[BOX_OBSTACLE_03] = float
+
     BOX_OBSTACLE_04 = 0xA7
+    PacketType[BOX_OBSTACLE_04] = float
+
     BOX_OBSTACLE_05 = 0xA8
+    PacketType[BOX_OBSTACLE_05] = float
 
     CYLINDER_OBSTACLE_02 = 0xAB
-    CYLINDER_OBSTACLE_03 = 0xAC
-    CYLINDER_OBSTACLE_04 = 0xAD
-    CYLINDER_OBSTACLE_05 = 0xAE
-
-    PacketType[BOX_OBSTACLE_02] = float
-    PacketType[BOX_OBSTACLE_03] = float
-    PacketType[BOX_OBSTACLE_04] = float
-    PacketType[BOX_OBSTACLE_05] = float
     PacketType[CYLINDER_OBSTACLE_02] = float
+
+    CYLINDER_OBSTACLE_03 = 0xAC
     PacketType[CYLINDER_OBSTACLE_03] = float
+
+    CYLINDER_OBSTACLE_04 = 0xAD
     PacketType[CYLINDER_OBSTACLE_04] = float
+
+    CYLINDER_OBSTACLE_05 = 0xAE
     PacketType[CYLINDER_OBSTACLE_05] = float
 
     # Monitor
-    VOLTAGE = 0x90 
-    INTERNAL_HUMIDITY = 0x65
-    INTERNAL_TEMPERATURE = 0x66 
-    INTERNAL_PRESSURE = 0x67
-    FACTORY_CLIMATE = 0x28
-    SOFTWARE_VERSION = 0x6C 
-    HARDWARE_STATUS_FLAG = 0x68
-    
+    VOLTAGE = 0x90
     PacketType[VOLTAGE] = float
-    PacketType[INTERNAL_HUMIDITY] = float
-    PacketType[INTERNAL_TEMPERATURE] = float
-    PacketType[INTERNAL_PRESSURE] = float
-    PacketType[FACTORY_CLIMATE] = float
-    PacketType[SOFTWARE_VERSION] = int
-    PacketType[HARDWARE_STATUS_FLAG] = int
-        
 
-    
-    
-    
+    INTERNAL_HUMIDITY = 0x65
+    PacketType[INTERNAL_HUMIDITY] = float
+
+    INTERNAL_TEMPERATURE = 0x66
+    PacketType[INTERNAL_TEMPERATURE] = float
+
+    INTERNAL_PRESSURE = 0x67
+    PacketType[INTERNAL_PRESSURE] = float
+
+    FACTORY_CLIMATE = 0x28
+    PacketType[FACTORY_CLIMATE] = float
+
+    SOFTWARE_VERSION = 0x6C
+    PacketType[SOFTWARE_VERSION] = int
+
+    HARDWARE_STATUS_FLAG = 0x68
+    PacketType[HARDWARE_STATUS_FLAG] = int
+
+
+
+
+
